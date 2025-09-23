e7dbe3daf966f1da001f425a3ab51799c812c757
TonNomGitHub <ton@email.com>
2025-09-23 17:30:22 +0100

    add test

---
+++ brief4/src/components/Test2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test2() {
+  return (
+    <div>
+      <h1 className='text-amber-800'>Houuuuuuusna</h1>
+    </div>
+  )
+}
+
+export default Test2
---
+++ brief4/src/pages/Publier.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Test2 from '../components/Test2'
+
+function Publier() {
+  return (
+    <div>
+      <Test2/>
+    </div>
+  )
+}
+
+export default Publier
